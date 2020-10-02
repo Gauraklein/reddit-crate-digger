cd154e0cd78e56c10f19178460bf3279cbc65731
gauraklein <gaurakklein@gmail.com>
2020-10-02 00:10:49 -0500

    basic skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const index = () => {
+  return (
+    <>
+    <h1>CRATE DIGGER</h1>
+    <p>Subreddit search bar will go here</p>
+    <p>content div and left - right controls will go here</p>
+    <p>random, newest, play, pause buttons go here</p>
+    </>
+  )
 }
+
+export default index
