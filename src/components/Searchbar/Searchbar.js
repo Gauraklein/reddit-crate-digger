9e851c33f67944b9e34d4ec08d504d8a90b73ac7
gauraklein <gaurakklein@gmail.com>
2020-10-10 22:54:30 -0500

    added css file and basic input as well as axios

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./styles.css"
+
+const Searchbar = () => {
+
+    return (
+        <>
+            <input />
+            <button>
+                Search
+                </button>
+        </>
+    )
+}
+
+export default Searchbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Searchbar from "../components/Searchbar/Searchbar"
 
 const index = () => {
   return (
     <>
     <h1>CRATE DIGGER</h1>
-    <p>Subreddit search bar will go here</p>
+    {/* <p>Subreddit search bar will go here</p> */}
+    <Searchbar />
     <p>content div and left - right controls will go here</p>
     <p>random, newest, play, pause buttons go here</p>
     </>
