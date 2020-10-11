978fe5632851ea1ee51e9e64ebb1d21b259530c3
gauraklein <gaurakklein@gmail.com>
2020-10-10 23:01:21 -0500

    added in a basic axios function, will implement when I have a bit more time

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,7 +1,18 @@
 import React from "react"
 import "./styles.css"
+import axios from "axios"
 
 const Searchbar = () => {
+
+    const handleSearch = (searchQuery) => {
+        axios({
+            method: 'get',
+            url: 'https://www.reddit.com/r/' + searchQuery + '.json',
+          })
+            .then(function(response) {
+            console.log("this is the response", response)
+          });
+    }
 
     return (
         <>
