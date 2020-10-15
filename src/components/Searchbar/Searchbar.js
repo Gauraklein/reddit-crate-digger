90acbf2435a9b56a05db997809f77a41353a3c42
gauraAdmin <gaurakklein@gmail.com>
2020-10-15 09:56:40 -0500

    got the json returning

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import "./styles.css"
 import axios from "axios"
 
 const Searchbar = () => {
+
+    const [searchBarValue, setSearchbarValue] = useState("")
 
     const handleSearch = (searchQuery) => {
         axios({
@@ -10,14 +12,19 @@ const Searchbar = () => {
             url: 'https://www.reddit.com/r/' + searchQuery + '.json',
           })
             .then(function(response) {
-            console.log("this is the response", response)
+            console.log("this is the response", response.data.data.children)
           });
     }
 
     return (
         <>
-            <input />
-            <button>
+            <input type="text" name="subreddit" value={searchBarValue} onChange={(e) => { 
+                setSearchbarValue(e.target.value)
+                console.log(searchBarValue)}}/>
+            <button onClick={(e) => {
+                e.preventDefault()
+                handleSearch(searchBarValue)
+            }}>
                 Search
                 </button>
         </>
