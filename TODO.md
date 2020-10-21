49f5b6b7cd7a2c0ccf53369e8c48d9f4d2584d56
gauraklein <gaurakklein@gmail.com>
2020-10-20 23:30:12 -0500

    formatting and added todo list

---
+++ TODO.md
@@ -0,0 +1,7 @@
+## A Place to track TODOS
+
+- look into the reddit api and how to return the next page of results
+- think about crate management and when to query for more 'records' also how to add records for multiple places
+- dealing with other types of media: soundcloud, spotify, etc
+- add a loading spinner
+- error handling
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,34 +1,40 @@
 import React from "react"
 import "./styles.css"
-import {useSelector, useDispatch } from "react-redux"
-import {handleSearchInput, handleSearch} from "./searchbarActions"
+import { useSelector, useDispatch } from "react-redux"
+import { handleSearchInput, handleSearch } from "./searchbarActions"
 
 const Searchbar = () => {
+  const dispatch = useDispatch()
+  const { searchQuery } = useSelector(state => state.searchbar)
 
-    const dispatch = useDispatch()
-    const { searchQuery } = useSelector(state => state.searchbar)
-
-    return (
-        <form onKeyPress={(e) => {
-
-            if (e.key === 'Enter') {
-                e.preventDefault()
-                dispatch(handleSearch(searchQuery))
-            }
-        }}>
-            <input type="text" name="subreddit" value={searchQuery} onChange={(e) => { 
-                dispatch(handleSearchInput(e.target.value))}}/>
-            <button 
-            type='submit'
-                onClick={(e) => {
-                    e.preventDefault()
-                    dispatch(handleSearch(searchQuery))}}
-                
-            >
-                Search
-                </button>
-        </form>
-    )
+  return (
+    <form
+      onKeyPress={e => {
+        if (e.key === "Enter") {
+          e.preventDefault()
+          dispatch(handleSearch(searchQuery))
+        }
+      }}
+    >
+      <input
+        type="text"
+        name="subreddit"
+        value={searchQuery}
+        onChange={e => {
+          dispatch(handleSearchInput(e.target.value))
+        }}
+      />
+      <button
+        type="submit"
+        onClick={e => {
+          e.preventDefault()
+          dispatch(handleSearch(searchQuery))
+        }}
+      >
+        Search
+      </button>
+    </form>
+  )
 }
 
 export default Searchbar
